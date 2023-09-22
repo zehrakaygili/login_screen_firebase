f41e3b90a4cab6c26328ae0dada212d07028a593
zehrakaygili <129604808+zehrakaygili@users.noreply.github.com>
2023-09-23 01:10:16 +0300

    login page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:registration_firebase/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:registration_firebase/login_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,10 +18,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
